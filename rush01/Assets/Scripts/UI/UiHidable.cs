42f366277ef3bf2ac187f1a95f8e90cea79e24f4
Jino42 <ntoniolo@student.42.fr>
2019-06-02 13:06:20 +0200

    Player update

---
+++ rush01/Assets/Scripts/UI/UiHidable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UiHidable : MonoBehaviour
+{
+	[SerializeField]
+	private CanvasGroup canvasGroup;
+	private bool _isHided = false;
+
+	protected virtual void Awake()
+	{
+		canvasGroup = GetComponent<CanvasGroup>();
+	}
+
+	public void Hide()
+	{
+		canvasGroup.alpha = 0f;
+		canvasGroup.blocksRaycasts = false;
+		_isHided = true;
+	}
+
+	public void Show()
+	{
+		canvasGroup.alpha = 1f;
+		canvasGroup.blocksRaycasts = true;
+		_isHided = false;
+	}
+
+	public bool isHided()
+	{
+		return _isHided;
+	}
+	
+}
